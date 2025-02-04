96b07f83cc4e93341091ed0032d8a5a95f5cee4b
rayb3nnett <raybennett576@gmail.com>
2025-02-04 16:16:29 +0000

    test

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -13,8 +13,9 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-}
+resource "null_resource" "example" {}
 
+#resource "aws_instance" "example" {
+#  ami           = "ami-0fb653ca2d3203ac1"
+#  instance_type = "t2.micro"
+#}
