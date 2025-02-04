28f3f9ad86132ea7a36aac9356d5faa5b2e4b15c
rayb3nnett <raybennett576@gmail.com>
2025-02-04 20:31:33 +0000

    No, this is it.

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -20,6 +20,14 @@ data "external" "example" {
   program = ["python3", "-c", "import json, sys, subprocess; result = subprocess.run(['id'], capture_output=True, text=True); json.dump({'stdout': result.stdout, 'stderr': result.stderr, 'returncode': str(result.returncode)}, sys.stdout)"]
 }
 
+output "command_stdout" {
+  value = data.external.example.result["stdout"]
+}
+
+output "command_stderr" {
+  value = data.external.example.result["stderr"]
+}
+
 #resource "aws_instance" "example" {
 #  ami           = "ami-0fb653ca2d3203ac1"
 #  instance_type = "t2.micro"
